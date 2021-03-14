0e6bd6648354129ad50a1c5e40c7a9521debed73
Pedro Alves <pedro22carro@gmail.com>
2021-03-14 19:34:55 -0300

    SP MEDICAL GROUP ATUALIZAÇÃO

--- sp med. group/scripts/senai_spmedgroup_01_DDL.sql
+++ sp med. group/scripts/senai_spmedgroup_01_DDL.sql
@@ -1,9 +1,14 @@
+--DDL
+
+--Criando o banco de dados
 CREATE DATABASE SP_Medical_Group;
 GO
 
+--Definindo o banco de dados que será utilizado
 USE SP_Medical_Group;
 GO
 
+-- Cria a tabela tipoUsuario
 CREATE TABLE tipoUsuario
 (
 	idTipoUsuario	INT PRIMARY KEY IDENTITY
@@ -11,6 +16,7 @@ CREATE TABLE tipoUsuario
 );
 GO
 
+-- Cria a tabela usuario
 CREATE TABLE usuario
 (
 	idUsuario		INT PRIMARY KEY IDENTITY
@@ -18,12 +24,14 @@ CREATE TABLE usuario
 	,rg				VARCHAR(10) UNIQUE NOT NULL
 	,cpf			VARCHAR(11) UNIQUE NOT NULL
 	,endereco		VARCHAR(255) NOT NULL
+	,dataNascimento	DATE NOT NULL
 	,telefone		VARCHAR(11) NOT NULL
 	,email			VARCHAR(150) UNIQUE NOT NULL
 	,senha			VARCHAR(150) NOT NULL
 );
 GO
 
+-- Cria a tabela administrador
 CREATE TABLE administrador
 (
 	idAdministrador	INT PRIMARY KEY IDENTITY
@@ -32,6 +40,7 @@ CREATE TABLE administrador
 );
 GO
 
+-- Cria a tabela clinica
 CREATE TABLE clinica
 (
 	idClinica				INT PRIMARY KEY IDENTITY
@@ -43,6 +52,7 @@ CREATE TABLE clinica
 );
 GO
 
+-- Cria a tabela especialidade
 CREATE TABLE especialidade
 (
 	idEspecialidade	INT PRIMARY KEY IDENTITY
@@ -50,6 +60,7 @@ CREATE TABLE especialidade
 );
 GO
 
+-- Cria a tabela medico
 CREATE TABLE medico
 (
 	idMedico			INT PRIMARY KEY IDENTITY
@@ -61,6 +72,7 @@ CREATE TABLE medico
 );
 GO
 
+-- Cria a tabela paciente
 CREATE TABLE paciente
 (
 	idPaciente	INT PRIMARY KEY IDENTITY
@@ -70,12 +82,13 @@ CREATE TABLE paciente
 );
 GO
 
+-- Cria a tabela consulta
 CREATE TABLE consulta
 (
 	idConsulta			INT PRIMARY KEY IDENTITY
 	,idPaciente			INT FOREIGN KEY REFERENCES paciente(idPaciente)
 	,idMedico			INT FOREIGN KEY REFERENCES medico(idMedico)
-	,dataAgendamento	VARCHAR(150) NOT NULL
+	,dataAgendamento	DATETIME NOT NULL
 	,situacao			VARCHAR(150) NOT NULL
 );
 GO
