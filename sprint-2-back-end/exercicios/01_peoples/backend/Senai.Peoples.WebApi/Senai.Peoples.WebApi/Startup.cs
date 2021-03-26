516a1d28355025b8221e83553b718741d1d560a0
Pedro Alves <pedro22carro@gmail.com>
2021-03-26 19:27:04 -0300

    Peoples

--- sprint-2-back-end/exercicios/01_peoples/backend/Senai.Peoples.WebApi/Senai.Peoples.WebApi/Startup.cs
+++ sprint-2-back-end/exercicios/01_peoples/backend/Senai.Peoples.WebApi/Senai.Peoples.WebApi/Startup.cs
@@ -16,6 +16,7 @@ namespace Senai.Peoples.WebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,9 +31,9 @@ namespace Senai.Peoples.WebApi
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                app.UseEndpoints(endpoints =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    endpoints.MapControllers();
                 });
             });
         }
